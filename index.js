60c9de51d4c419b74060eb1c966524110c003c59
Aghazzi <aghazzi.1998@gmail.com>
2022-12-28 16:41:44 +0200

    step 3

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.get("/test", (req, res) => {
+    res.send({ status: 200, message: "ok" });
+});
+
+app.get("/time", (req, res) => {
+    let date = new Date();
+    res.send({
+        status: 200,
+        message: `${date.getHours()}:${date.getMinutes()}`,
+    });
+});
+
 app.get("/", (req, res) => {
     res.send("ok");
 });
