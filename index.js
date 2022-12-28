acf2075794b9567e08f865719806a0ba8019c6b1
Aghazzi <aghazzi.1998@gmail.com>
2022-12-28 17:07:29 +0200

    step 5

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const movies = [
+    { title: "Jaws", year: 1975, rating: 8 },
+    { title: "Avatar", year: 2009, rating: 7.8 },
+    { title: "Brazil", year: 1985, rating: 8 },
+    { title: "الإرهاب والكباب", year: 1992, rating: 6.2 },
+];
 
 app.get("/test", (req, res) => {
     res.send({ status: 200, message: "ok" });
@@ -31,6 +37,22 @@ app.get("/search", (req, res) => {
             message: "you have to provide a search",
         });
     }
+});
+
+app.get("/movies/create", (req, res) => {
+    res.send();
+});
+
+app.get("/movies/read", (req, res) => {
+    res.send({ status: 200, data: movies });
+});
+
+app.get("/movies/update", (req, res) => {
+    res.send();
+});
+
+app.get("/movies/delete", (req, res) => {
+    res.send();
 });
 
 app.get("/", (req, res) => {
